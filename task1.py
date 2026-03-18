c3959a68f551acb182d634eb6b3d4e88571934f5
mox098 <moxasangani1@gmail.com>
2026-03-18 18:47:56 +0000

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,22 @@
+a=14
+b=36
+c=98
+
+if a and b and c :
+    print("all numbers boolean value is true")
+else:
+    print ("all number boolean value is false")
+
+
+a=10
+b=20
+c=31
+if a> 0 or c>0:
+    print("one of the values is greater than 0")
+else:
+    print("values lower than 0")
+if b>0 or c>0:
+    print("either of values greater than 0")
+else:
+    print("no values is greater than 0")
+    
---
+++ task2.py
@@ -0,0 +1,23 @@
+a=9
+b=8
+c=10
+print(a!=b)
+print(b!=c)
+
+a="python"
+b="coding"
+ 
+if a!=b:
+    print(a,"and" ,b,"are differant.")
+
+a=8
+b=9
+
+if (a==2)!=(b==3):
+    print("codingal")
+
+a=int(input("enter a number"))
+if a%2==0:
+    print(a," is a even number")
+else:
+    print(a," is an odd number")
